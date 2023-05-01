b6fc2452ecda4fda2496311dc316177003f7752f
Fred <123298689+daily-fred@users.noreply.github.com>
2023-05-01 11:05:13 +0200

    new solution just dropped | lab 07: pass by reference
    * solved this lab today, full test coverage
    * not everything is solved by pass by reference [!!!]
    - this means it'll work but not be 100% correct, maybe will update later
    * not commented yet
    
    :)

---
+++ lab/lab7/q1-fred.c
@@ -0,0 +1,34 @@
+#define CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int minimum(const int pInt[], int i) {
+
+    int min = pInt[0];
+
+    int j;
+    j = 0;
+
+    while (j < i) {
+
+        if (pInt[j] < min) {
+
+            min = pInt[j];
+
+        }
+
+        j++;
+
+    }
+
+    return min;
+
+}
+
+int main(void) {
+
+    int numbers[] = {4, 6, 9, 5};
+    printf("%d", minimum(numbers, 4));
+
+}
+
---
+++ lab/lab7/q5-fred.c
@@ -0,0 +1,24 @@
+#define CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#define PI 3.1415
+
+void sphere(int radius, double *volume, double *surface) {
+
+    *volume = (4.0/3.0) * PI * radius * radius * radius;
+
+    *surface = 4.0 * PI * radius * radius;
+
+}
+
+int main(void) {
+
+    int radius = 5;
+    double volume, surface;
+    sphere(radius, &volume, &surface);
+    printf("Radius: %d\nVolumen: %.1lf\nOberflaeche %.1lf\n", radius, volume, surface);
+
+}
+
+
