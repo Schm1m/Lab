db487770ec17148e33216fe2ed17bf7e9a72ea3c
Schm1m <tim@schm1m.com>
2023-05-09 09:25:40 +0200

    i struggle with reading plain text

--- lab/lab8/q3-tim.c
+++ lab/lab8/q3-tim.c
@@ -3,13 +3,18 @@
 #include <stdio.h>
 
 int faculty(int);  // function prototype
+int input(void);   // -
 
 int main(int argc, char *argv[]) {
-    int i;                              // initiate i
-    scanf("%d", &i);                    // take input for i
-    printf("%d! = %d", i, faculty(i));  // print factorial of i
+    printf("%d! = %d", i, faculty(input()));  // print factorial of i
 }
 
 int faculty(int n) {
     return n == 1 ? n : n * faculty(n - 1);  // re-initiate function untill n reaches 1
+}
+
+int input(void) {
+    int i;            // initiate i
+    scanf("%d", &i);  // take input for i
+    return i;
 }
